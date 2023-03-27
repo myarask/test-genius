35752f47c908aa9614346bd7668534679782732a
Mark Yaraskavitch <36746340+myarask@users.noreply.github.com>
2023-03-27 13:40:27 -0400

    node wrapper

---
+++ cli/index.js
@@ -0,0 +1,23 @@
+const { execFile } = require('child_process');
+
+const runRustCli = (args, callback) => {
+  const rustCliPath = 'path-to-your-binary';
+  execFile(rustCliPath, args, callback);
+};
+
+if (require.main === module) {
+  const args = process.argv.slice(2);
+  runRustCli(args, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing Rust CLI: ${error}`);
+      return;
+    }
+    if (stderr) {
+      console.error(stderr);
+      return;
+    }
+    console.log(stdout);
+  });
+}
+
+module.exports = runRustCli;
